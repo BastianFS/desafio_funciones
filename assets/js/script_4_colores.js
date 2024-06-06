98bae7168040176cbaf609761afde8e5acf2a98e
Bastian Flores <basti.flosan@gmail.com>
2024-06-06 19:46:21 -0400

    Ejercicio 4.1

---
+++ assets/js/script_4_colores.js
@@ -0,0 +1,31 @@
+let div1 = document.getElementById("cont1");
+function pintar1(color){
+    div1.style.backgroundColor = color;
+}
+div1.addEventListener ("click", function(){
+    pintar1("black");
+})
+
+let div2 = document.getElementById("cont2");
+function pintar2(color){
+    div2.style.backgroundColor = color;
+}
+div2.addEventListener ("click", function(){
+    pintar2("black");
+}) 
+
+let div3 = document.getElementById("cont3");
+function pintar3(color){
+    div3.style.backgroundColor = color;
+}
+div3.addEventListener ("click", function(){
+    pintar3("black");
+})
+
+let div4 = document.getElementById("cont4");
+function pintar4(color){
+    div4.style.backgroundColor = color;
+}
+div4.addEventListener ("click", function(){
+    pintar4("black");
+})  
