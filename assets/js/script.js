e72c40f199630aeacd65b79b71011847cbfa1146
Bastian Flores <basti.flosan@gmail.com>
2024-06-06 18:26:18 -0400

    Ejercicio 3.2

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,4 @@
-function pintar(){
-    ele.style.backgroundColor = 'yellow'
-    }
     const ele = document.getElementById("ele1")
-    ele.addEventListener("click", pintar);
+    ele.addEventListener("click", function(){
+        ele.style.backgroundColor = "yellow"
+    });
